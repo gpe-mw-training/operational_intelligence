34eef721aaef89bd9b2a6fb9b4621ea4d54d6587
Praveen Kumar KS <35951257+Pkrish15@users.noreply.github.com>
2018-08-21 03:35:18 +0530

    Update KafkaProducerExample.scala

--- uber-kafka-streaming/src/main/scala/com/sparkkafka/uber/KafkaProducerExample.scala
+++ uber-kafka-streaming/src/main/scala/com/sparkkafka/uber/KafkaProducerExample.scala
@@ -56,7 +56,7 @@ object KafkaProducerExample extends App {
   import org.apache.spark.streaming.kafka.producer._
 
   val Array(topics) = args
-  val kafkaBrokers = "maprdemo:9092"
+  val kafkaBrokers = "localhost:9092"
   val numMessages = 10
 
   val batchTime = Seconds(2)
